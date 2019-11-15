102126b55e69be4ce58add0efb1e4b126681f0df
nikeshlama <nikeshlama76@gmail.com>
2019-11-15 08:39:27 +0545

    Initial commit

---
+++ app/src/main/java/com/e/email/GridViewLayoutActivity.java
@@ -0,0 +1,14 @@
+package com.e.email;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GridViewLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_view_layout);
+    }
+}
